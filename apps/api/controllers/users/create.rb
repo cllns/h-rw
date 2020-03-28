9258ae42149497a8b8aae782ef7ba920c3997674
Sean Collins <sean@cllns.com>
2020-03-28 13:09:00 -0600

    Updated users#create action to have nested keys
    A bit further, these are they keys that are expected within 'user',
    which will be updated to include the associated values from the newly
    created user (which hasn't been implemented yet).

--- apps/api/controllers/users/create.rb
+++ apps/api/controllers/users/create.rb
@@ -5,7 +5,21 @@ module Api
         include Api::Action
 
         def call(_params)
-          self.body = Hash[user: {}].to_json
+          self.body = Hash[
+            user: user_hash
+          ].to_json
+        end
+
+        private
+
+        def user_hash
+          Hash[
+            email: "",
+            username: "",
+            bio: "",
+            image: "",
+            token: "",
+          ]
         end
       end
     end
--- spec/api/controllers/users/create_spec.rb
+++ spec/api/controllers/users/create_spec.rb
@@ -6,6 +6,12 @@ RSpec.describe Api::Controllers::Users::Create, type: :action do
 
   it "is successful" do
     expect(response[0]).to eq 200
+  end
+
+  it "has appropriate keys" do
     expect(body.keys).to eq(["user"])
+    expect(body["user"].keys).to match_array(
+      %w[email username bio image token]
+    )
   end
 end
