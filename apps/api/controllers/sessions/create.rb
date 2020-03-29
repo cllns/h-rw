080b450825d3ffc20ce40ed719621ceacdb508f6
Sean Collins <sean@cllns.com>
2020-03-29 16:36:34 -0600

    Added session#create action, at /users/login
    Using our new User#find_by_email_and_password method

--- apps/api/controllers/sessions/create.rb
+++ apps/api/controllers/sessions/create.rb
@@ -4,7 +4,35 @@ module Api
       class Create
         include Api::Action
 
+        params do
+          required(:user).schema do
+            required(:email).filled(:str?)
+            required(:password).filled(:str?)
+          end
+        end
+
         def call(params)
+          if params.valid?
+            self.body = Hash[user: user_hash].to_json
+          else
+            self.status = 422
+            self.body = Hash[errors: params.errors.to_h].to_json
+          end
+        end
+
+        private
+
+        def user_hash
+          retrieve_user.to_hash.slice(
+            :email, :username, :bio, :image
+          ).merge(token: "")
+        end
+
+        def retrieve_user
+          UserRepository.new.find_by_email_and_password(
+            email: params.dig(:user, :email),
+            password: params.dig(:user, :password)
+          )
         end
       end
     end
--- spec/api/controllers/sessions/create_spec.rb
+++ spec/api/controllers/sessions/create_spec.rb
@@ -1,9 +1,52 @@
 RSpec.describe Api::Controllers::Sessions::Create, type: :action do
   let(:action) { described_class.new }
-  let(:params) { Hash[] }
+  let(:response) { action.call(params) }
+  let(:body) { JSON.parse(response[2][0]) }
 
-  it "is successful" do
-    response = action.call(params)
-    expect(response[0]).to eq 200
+  before { UserRepository.new.clear }
+
+  describe "with invalid params" do
+    let(:params) { Hash[] }
+
+    it "is 422: unprocessable entity" do
+      expect(response[0]).to eq 422
+      expect(body).to eq(
+        Hash[
+          "errors" => {
+            "user" => ["is missing"]
+          }
+        ]
+      )
+    end
+  end
+
+  describe "with valid params" do
+    before do
+      UserRepository.new.create(
+        email: "test@example.com",
+        username: "tester",
+        encrypted_password: BCrypt::Password.create("password")
+      )
+    end
+
+    let(:params) do
+      Hash[
+        user: {
+          email: "test@example.com",
+          password: "password"
+        }
+      ]
+    end
+
+    it "is successful" do
+      expect(response[0]).to eq 200
+    end
+
+    it "has appropriate response" do
+      expect(body["user"]).to include(
+        "email" => "test@example.com",
+        "username" => "tester"
+      )
+    end
   end
 end
